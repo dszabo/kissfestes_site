eb3396cf46288cb7edf03f790e80058e6ed506ce
dszabo <dszabo@gmail.com>
2015-02-01 20:15:20 +0100

    initial commit

---
+++ js/cufon-replace.js
@@ -0,0 +1,7 @@
+//Cufon.replace('.menu > li > a', { fontFamily: 'KleinSlabserifBlaxX', hover:true });
+//Cufon.replace('.banner strong, .menu li li a, h3, h4, h5, h6, .link-1', { fontFamily: 'KleinSlabserif-Light', hover:true });
+//Cufon.replace('.text-1, .text-2', { fontFamily: 'Lane - Narrow', hover:true });
+
+Cufon.replace('.menu > li > a', { fontFamily: 'Dosis', hover:true });
+Cufon.replace('.banner strong, .menu li li a, h3, h4, h5, h6, .link-1', { fontFamily: 'Dosis', hover:true });
+Cufon.replace('.text-1, .text-2', { font-family: 'Dosis', hover:true });
